211b873137ade0080bb3483d80efdf367c5cab20
Georgee Tomii <tomiigeorgee@gmail.com>
2025-05-23 20:53:50 +0300

    add some styles to about page'

---
+++ views/about.blade.php
@@ -0,0 +1,18 @@
+@extends('layout')
+
+@section('content')
+    
+<div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+    <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
+       ABOUT US
+    </div>
+
+            <div class="mt-8 dart:bg-gray-000 overflow-hidden">
+                THIS IS THE ABOUT PAGE
+            </div>
+
+@endsection
+
+@section('title' , 'about')
+    
+{{-- @endsection --}}
